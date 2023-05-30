bd564531e4c89b37c68cd881e1524ebc4ca2a7ae
Tec <Tec@SKAB4-PC8>
2023-05-30 12:53:16 +0200

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp8.codes;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FilHandler filHandler = new();
+            filHandler.CreateFile();
+        }
+    }
+}
---
+++ ConsoleApp8/codes/FilHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8.codes
+{
+    internal class FilHandler
+    {
+        public bool CreateFile()
+        {
+            bool success = true;
+
+            try
+            {
+                string myUserFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                File.Create(Path.Combine(myUserFolder, "AndreasDarab.txt"));
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+
+            return success;
+        }
+    }
+}
